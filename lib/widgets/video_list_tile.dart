ab993d35b78cccd49496e6611eb9101953a5cfcc
badayvedat <badayvedat@gmail.com>
2020-10-09 22:01:10 +0300

    video_list_tile implemented

---
+++ lib/widgets/video_list_tile.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class VideoListTile extends StatelessWidget {
+  final String path;
+
+  VideoListTile(this.path);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: EdgeInsetsDirectional.only(start: 9),
+      leading: Icon(
+        Icons.video_collection_rounded,
+        size: 50,
+      ),
+      title: Text(path.split('/').last),
+      // TODO implement onTap
+      onTap: () {},
+    );
+  }
+}
